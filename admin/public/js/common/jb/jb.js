4fb920cc3a846350be9d27b98604dc21a0e228fa
vinesh <vineshhpatel@gmail.com>
2015-06-05 16:47:01 +0530

    adding tooptip icon for label, code refactor

--- admin/public/js/common/jb/jb.js
+++ admin/public/js/common/jb/jb.js
@@ -9,46 +9,53 @@ jQuery.noConflict();
 		var getDataObjectForLabel = function(labelForWhichTooltipDataNeeded, listOfTooltipObjects) {
 
 			for (var i = listOfTooltipObjects.length - 1; i >= 0; i--) {
-				console.log('Key - ', listOfTooltipObjects[i].key);
 				if(listOfTooltipObjects[i].key.toLowerCase() == labelForWhichTooltipDataNeeded.toLowerCase()) {
-					console.log('Returning listOfTooltipObject - ', listOfTooltipObjects[i]);
 					return listOfTooltipObjects[i];
 				}
 			};
-			return "Entry not found. Contact Master Admin."
+			return null;
 		}
 			
 		// Getting model name from the SINGULAR PROPERTY of 
 		var modelName = Keystone.list.singular.replace(/\s/g, '');
-		console.log('Modal for Page - ', modelName);
 		
 		// Fetching All Details
-		console.log('Loading Data Sets for Tooltip File....');
+		console.log('Loading Data Sets for Tooltip File for Model Name - ', modelName);
 		$.ajax({
 			url: "/api/getTooltips/" + modelName,
 			data: {
 				modelName: modelName
 			}
 		}).done(function(response) {
-			console.log('Data receieved from Server - ', response);
+			console.log('Data receieved from Server for Tooltip - ', response);
 
 			// Finding all Labels
-			// var elements = document.querySelectorAll('[data-field-path]');
 			var elements = document.getElementsByClassName('field-label');
-			console.log('Getting elements - ', elements);
 			for (var i = elements.length - 1; i >= 0; i--) {
 				
+				var dataForLabelFromMap = getDataObjectForLabel($(elements[i]).text(), response.data);
+				if(dataForLabelFromMap == null) {
+					continue;
+				}
+				console.log(dataForLabelFromMap);
+
+				// For each Row Add Icon at end
+				$(elements[i]).append('&nbsp;&nbsp;<span id="tooltip-'+i+'" data="'+$(elements[i]).text()+'" class="ion-information-circled"></span>');
+
+
 				// For each Row find LABEL & assigning on Click of that
-				$(elements[i]).on('click', {
-					elements: elements,
-					i: i
+				$('#tooltip-'+ i).on('click', {
+					dataForLabel: dataForLabelFromMap
 				}, function(event){
 
-					var dataForLabel = getDataObjectForLabel($(event.data.elements[event.data.i]).text(), response.data);
+					var dataForLabel = event.data.dataForLabel;
 
+					// Forming Content for Modal
 					var popupContentForModal = dataForLabel.popupContent;
 					if(dataForLabel.popupImage) {
-						popupContentForModal += " <br/> <div style='text-align: center;'><img src='" + dataForLabel.popupImage.url + "'/></div> <br/>";
+						if(dataForLabel.popupImage.url.length > 3) {
+							popupContentForModal += " <br/> <div style='text-align: center;'><img src='" + dataForLabel.popupImage.url + "'/></div> <br/>";
+						}
 					}
 					if(dataForLabel.popupVideo) {
 						popupContentForModal += " <br/> Video Url: " + dataForLabel.popupVideo;
