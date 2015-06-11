e76de11378648b15a22660306390fec7e847d411
vinesh <vineshhpatel@gmail.com>
2015-06-11 14:32:18 +0530

    restricted image width in popup

--- admin/public/js/common/jb/jb.js
+++ admin/public/js/common/jb/jb.js
@@ -54,7 +54,7 @@ jQuery.noConflict();
 					var popupContentForModal = dataForLabel.popupContent;
 					if(dataForLabel.popupImage) {
 						if(dataForLabel.popupImage.url.length > 3) {
-							popupContentForModal += " <br/> <div style='text-align: center;'><img src='" + dataForLabel.popupImage.url + "'/></div> <br/>";
+							popupContentForModal += " <br/> <div style='text-align: center;'><img style='max-width: 250px;' src='" + dataForLabel.popupImage.url + "'/></div> <br/>";
 						}
 					}
 					if(dataForLabel.popupVideo) {
